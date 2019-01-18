38b255e99147e46ad795140f950b521e340097bb
jiangmoua <46395773+jiangmoua@users.noreply.github.com>
2019-01-18 08:59:20 +0800

    第一次

--- src/main/java/com/example/demo/TestgitApplication.java
+++ src/main/java/com/example/demo/TestgitApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestgitApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {//三生三世
 		SpringApplication.run(TestgitApplication.class, args);
 	}
 
